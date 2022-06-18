f06231bed9644817f66033c36de0183baa07e2b3
Fayadh al-Mosawi <fayadh.almosawi@gmail.com>
2022-06-18 15:18:19 -0400

    Remove use of BreadcrumbsProvider in App index

--- client/source/index.tsx
+++ client/source/index.tsx
@@ -1,31 +1,29 @@
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from "./serviceWorker";
 
-import { BreadcrumbsProvider } from '@common/providers/breadcrumbsProvider';
+import { BreadcrumbsProvider } from "@common/providers/breadcrumbsProvider";
 
-import DateAdapter from '@mui/lab/AdapterMoment';
-import ErrorBoundary from '@common/components/ErrorBoundary';
-import LocalizationProvider from '@mui/lab/LocalizationProvider';
-import { Pages } from '@layouts/Pages';
-import React from 'react';
-import { StyledEngineProvider } from '@mui/material/styles';
-import { Theme } from '@common/theme';
-import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import DateAdapter from "@mui/lab/AdapterMoment";
+import ErrorBoundary from "@common/components/ErrorBoundary";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import { Pages } from "@layouts/Pages";
+import React from "react";
+import { StyledEngineProvider } from "@mui/material/styles";
+import { Theme } from "@common/theme";
+import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
 const App: React.FC = () => (
-    <BreadcrumbsProvider>
-        <BrowserRouter basename="/">
-          <Theme>
-            <StyledEngineProvider injectFirst>
-              <LocalizationProvider dateAdapter={DateAdapter}>
-                <ErrorBoundary>
-                  <Pages />
-                </ErrorBoundary>
-              </LocalizationProvider>
-            </StyledEngineProvider>
-          </Theme>
-        </BrowserRouter>
-    </BreadcrumbsProvider>
+  <BrowserRouter basename="/">
+    <Theme>
+      <StyledEngineProvider injectFirst>
+        <LocalizationProvider dateAdapter={DateAdapter}>
+          <ErrorBoundary>
+            <Pages />
+          </ErrorBoundary>
+        </LocalizationProvider>
+      </StyledEngineProvider>
+    </Theme>
+  </BrowserRouter>
 );
 
 (async () => {
@@ -33,7 +31,7 @@ const App: React.FC = () => (
     <React.StrictMode>
       <App />
     </React.StrictMode>,
-    document.querySelector('#root'),
+    document.querySelector("#root")
   );
 })();
 
