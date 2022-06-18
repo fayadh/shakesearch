f8f359da98c0381c85a5773b499629c6138f5a00
Fayadh al-Mosawi <fayadh.almosawi@gmail.com>
2022-06-18 14:16:47 -0400

    Delete unused files

--- client/source/common/forms/inputs/Select/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import { Select as MuiSelect, SelectProps } from '@mui/material';
-
-export const Select = <T,>(props: SelectProps<T>) => (
-  <MuiSelect disableUnderline variant={'standard'} {...props}></MuiSelect>
-);
