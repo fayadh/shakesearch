a57b5d0fcb9d0be4c77dfaa2fe828fce87617d61
Fayadh al-Mosawi <fayadh.almosawi@gmail.com>
2022-06-18 20:50:25 -0400

    Add client build

---
+++ client/build/precache-manifest.1c6cf578316e2ad2e5b57d8a7cc84a41.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82ee6b5133e1bf7c22acfe02c4d9ba77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50ffdeeea34dd8a27dbc",
+    "url": "/static/js/2.bd887731.chunk.js"
+  },
+  {
+    "revision": "b56871e245f1596d6b18f6c251fd685d",
+    "url": "/static/js/2.bd887731.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ce32519795f2e06dafa",
+    "url": "/static/js/main.089eec74.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.089eec74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "254fe744f2f1926274e8",
+    "url": "/static/js/runtime-main.79d52560.js"
+  },
+  {
+    "revision": "fb41e1f09952dac27a3e71dcdb004be8",
+    "url": "/static/media/close.20ce81e8.svg"
+  },
+  {
+    "revision": "394ef14cf5b031cb10b41b7aaee4311b",
+    "url": "/static/media/search.0c19462f.svg"
+  }
+]);
