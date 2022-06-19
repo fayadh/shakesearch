7cc70a82082ea64c2b544fc9a6f9b7f124db4384
Fayadh al-Mosawi <fayadh.almosawi@gmail.com>
2022-06-19 10:03:21 -0400

    Remove unsued files

--- client/source/layouts/types.ts
+++
@@ -1,9 +0,0 @@
-import { RouteProps } from 'react-router-dom';
-
-import { FeatureFlag } from '@common/constants/featureFlag';
-
-export interface IAuthRouteProps extends RouteProps {
-  featureFlag?: FeatureFlag;
-  // Indicates if the user is authenticated into the application or not
-  isAuthenticated: boolean;
-}
