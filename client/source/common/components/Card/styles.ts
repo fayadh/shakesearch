22a15be27975331b3becc8e97b22abccf5c2f791
Fayadh al-Mosawi <fayadh.almosawi@gmail.com>
2022-06-21 11:35:05 -0400

    Correct Card styles

--- client/source/common/components/Card/styles.ts
+++ client/source/common/components/Card/styles.ts
@@ -1,16 +1,12 @@
-import makeStyles from '@mui/styles/makeStyles';
+import makeStyles from "@mui/styles/makeStyles";
 
 export const useStyles = makeStyles((theme) => ({
   content: {
-    padding: theme.spacing(1),
-    paddingLeft: theme.spacing(2),
-    paddingRight: theme.spacing(2),
+    padding: theme.spacing(2),
   },
   title: {
-    fontSize: '15px',
+    fontSize: "15px",
     fontWeight: 400,
     padding: theme.spacing(2),
-    paddingLeft: theme.spacing(3),
-    paddingRight: theme.spacing(3),
   },
 }));
