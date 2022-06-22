9533b4485c11b83eaf25fc3413ab73942afbce36
Fayadh al-Mosawi <fayadh.almosawi@gmail.com>
2022-06-22 16:43:43 -0400

    Remove extra console.log

--- client/source/common/constants/environment.ts
+++ client/source/common/constants/environment.ts
@@ -1,9 +1,6 @@
 const BASE_URL = "http://localhost:3001";
 const PRODUCTION_URL = "https://shakesearch-app.herokuapp.com";
 
-console.log("server url", process.env.SERVER_URL);
-console.log("process.env", process.env);
-
 export const ENV = {
   serverURL: process.env.NODE_ENV === "production" ? PRODUCTION_URL : BASE_URL,
 };
