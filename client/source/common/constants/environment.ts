eb9ff95a232c219f47a49cc55849b6a843ccb4c1
Fayadh al-Mosawi <fayadh.almosawi@gmail.com>
2022-06-22 16:26:40 -0400

    Add production url

--- client/source/common/constants/environment.ts
+++ client/source/common/constants/environment.ts
@@ -1,8 +1,9 @@
 const BASE_URL = "http://localhost:3001";
+const PRODUCTION_URL = "https://shakesearch-app.herokuapp.com";
 
 console.log("server url", process.env.SERVER_URL);
 console.log("process.env", process.env);
 
 export const ENV = {
-  serverURL: process.env.SERVER_URL ?? BASE_URL,
+  serverURL: process.env.NODE_ENV === "production" ? PRODUCTION_URL : BASE_URL,
 };
