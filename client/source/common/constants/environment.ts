16cde1348448e9aa8dc246cefa17ea92b1af9c4b
Fayadh al-Mosawi <fayadh.almosawi@gmail.com>
2022-06-22 16:22:53 -0400

    Add some logs

--- client/source/common/constants/environment.ts
+++ client/source/common/constants/environment.ts
@@ -1,5 +1,8 @@
 const BASE_URL = "http://localhost:3001";
 
+console.log("server url", process.env.SERVER_URL);
+console.log("process.env", process.env);
+
 export const ENV = {
   serverURL: process.env.SERVER_URL ?? BASE_URL,
 };
