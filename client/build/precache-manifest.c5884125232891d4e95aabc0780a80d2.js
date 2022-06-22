7418e05e4a3979f0523c4fd7580fc5d381e75530
Fayadh al-Mosawi <fayadh.almosawi@gmail.com>
2022-06-22 16:27:06 -0400

    Update build

---
+++ client/build/precache-manifest.c5884125232891d4e95aabc0780a80d2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "377d3765a3b0052268344f81eb3c6230",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5d101abdf33730d52a2",
+    "url": "/static/js/2.4aa88ee3.chunk.js"
+  },
+  {
+    "revision": "9ca2229f66378bdadf05a175fff555d6",
+    "url": "/static/js/2.4aa88ee3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b58372160ea42a4eb121",
+    "url": "/static/js/main.7d31ea36.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.7d31ea36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "254fe744f2f1926274e8",
+    "url": "/static/js/runtime-main.79d52560.js"
+  },
+  {
+    "revision": "fb41e1f09952dac27a3e71dcdb004be8",
+    "url": "/static/media/close.20ce81e8.svg"
+  },
+  {
+    "revision": "394ef14cf5b031cb10b41b7aaee4311b",
+    "url": "/static/media/search.0c19462f.svg"
+  }
+]);
